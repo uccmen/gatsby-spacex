4a71a4b3eb2ee6ef8c9344d90688c20727c72047
Ucchishta Sivaguru <ucc.men@gmail.com>
2019-07-20 02:44:24 +0800

    add github project link

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,15 +9,15 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to my Gatsby PlayDay project.</p>
-    <p>Pages below contains unofficial SpaceX data queried with GraphQL at
-      <a href={'https://github.com/spacexland/api'}> https://api.spacex.land/graphql/</a>
+    <p>Welcome to my Gatsby PlayDay <a href={'https://github.com/uccmen/gatsby-spacex'}>project</a>.</p>
+    <p>Pages below contains unofficial SpaceX data queried with GraphQL at<span> </span>
+       <a href={'https://github.com/spacexland/api'}>https://api.spacex.land/graphql/</a>
     </p>
     <ul>
-      <li><Link to="/launchpads/">Launch pads</Link></li>
       <li><Link to="/landingpads/">Landing pads</Link></li>
-      <li><Link to="/ships/">Ships</Link></li>
+      <li><Link to="/launchpads/">Launch pads</Link></li>
       <li><Link to="/launches/">Launches</Link></li>
+      <li><Link to="/ships/">Ships</Link></li>
     </ul>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem`, display: 'float' }}>
       <Image />
